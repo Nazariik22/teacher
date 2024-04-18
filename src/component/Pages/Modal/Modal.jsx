62e4f5a70ee9e5bc3d2cc04abd0fc3da4e8a8b4a
Nazarii Krutiak <nazariik20@gmail.com>
2024-04-18 23:03:35 +0300

    v4

--- src/component/Pages/Modal/Modal.jsx
+++ src/component/Pages/Modal/Modal.jsx
@@ -1,10 +1,25 @@
+import { useSelector } from "react-redux";
+import style from './Modal.module.css';
+import { NavLink } from "react-router-dom";
 
-const Modal = (props) => {
+const Modal = () => {
+    const state = useSelector(state => state.pages.modal)
     return (
-        <main>
-           
-            
+        <main className={style.main}>
+            {state.map(item => <div className={style.column}>
+                <h2>{item.title}</h2>
+                <section className={style.section}>
+                    <div className={`img ${style.img}`}>
+                        <img src={item.img} alt="" />
+                    </div>
+                    <div className={style.blok}><p>{item.text}</p>
+                    <NavLink to={item.link} className={style.button}>Детальніше</NavLink>
+                    </div>
+                </section>
+               
+            </div>)}
+
         </main>
     )
 }
-export {Modal}
+export { Modal }
