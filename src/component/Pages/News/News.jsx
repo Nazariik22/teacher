56b2f7df15f0aadd9d57f2469d5b107db70098f3
Nazarii Krutiak <nazariik20@gmail.com>
2024-04-18 23:14:02 +0300

    v4

--- src/component/Pages/News/News.jsx
+++ src/component/Pages/News/News.jsx
@@ -1,7 +1,10 @@
-
+import style from './News.module.css';
+import img from "./../../../img/progress.png";
 const News = (props) => {
     return (
-        <main>
+        <main className={style.main}>
+            <img src={img} alt="" />
+            <p>Наразі нових новин не має, поверніться пізніше</p>
            
             
         </main>
